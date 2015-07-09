fc4e162c2b7dddfe519591f3bb4a40c89fb9e5d3
Kim Mattiko <amberlie@rocketmail.com>
2015-07-09 17:38:21 -0400

    continuing to hook things up, 404 page exists and js file renamed

--- app/scripts/burybox.js
+++ app/scripts/burybox.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+/* global angular */
+(function(window) {
+  'use strict';
+
+angular.module('burybox', ['ngRoute', 'restangular'])
+  .config(function($routeProvider) {
+    $routeProvider.when('/landing', {
+      templateUrl: 'views/questions.html'
+    });
+    $routeProvder.when('/makebox', {
+      templateUrl: 'views/makebox.html'
+    });
+    $routeProvider.when('/viewbox', {
+      templateUrl: 'views/viewbox'
+    });
+    $routeProvider.otherwise('/404');
+  });
+
+})(window);
