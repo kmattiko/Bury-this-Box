dbbffdc609ceba74d571f0712a81cbcd667be253
Kim Mattiko <amberlie@rocketmail.com>
2015-07-15 16:32:33 -0400

    getting pic url to add to object in array thanks to help from @sjoyal

--- app/scripts/controllers/addphoto.js
+++ app/scripts/controllers/addphoto.js
@@ -2,67 +2,24 @@
 (function() {
   'use strict';
 
-/*angular.module('appFilereader', []).directive('appFilereader', function($q){
-  var slice = Array.prototype.slice;
-
-  return {
-    restrict: 'A',
-    require: '?ngModel',
-    link: function(scope, element, attrs, ngModel) {
-      if (!ngModel) return;
-
-      ngModel.$render = function() {};
-
-      element.bind('change', function(e) {
-        var element = e.target;
-
-        $q.all(slice.call(element.files, 0).map(readFile))
-        .then(function(values) {
-          if (element.multiple) ngModel.$setViewValue(values);
-          else ngModel.$setViewValue(values.length ? values[0] : null);
-
-        });
-
-        function readFile(file) {
-          var deferred = $q.defer();
-
-          var reader = new FileReader();
-          reader.onload = function(e) {
-            deferred.resolve(e.target.result);
-          };
-          reader.onerror = function(e) {
-            deferred.reject(e);
-          };
-          reader.readAsDataURL(file);
-
-          return deferred.promise;
-        }
+    angular.module('burybox')
+    .controller('PhotoController', function($scope) {
+      $scope.list = [];
+      $scope.someText = {
+        testField: ''
+      };
+      // $scope.text = 'howdy';
+      $scope.submit = function() {
+          $scope.list.push($scope.someText);
+          $scope.someText = { testField: ''};
+        };
       });
-    }
-  };
-});*/
+      //on NG-CLick of submit button img needs to convert to
+    //  console.log("test")
+    //  var selectedFile = $('#input').get(0).file[0];
 
 
 
-
-/*.directive('fileread', [function () {
-  return {
-    scope: {
-      fileread: "="
-    };
-    link: function (scope, element, attributes) {
-      element.bind("change", function (changeEvent) {
-        var reader = new FileReader();
-        reader.onload = function (loadEvent) {
-          scope.$apply(function () {
-            scope.fileread = loadEvent.target.result;
-          });
-        }
-        reader.readAsDataURL(changeEvent.target.files[0]);
-      });
-    }
-  }
-}]);*/
 
 
 /*  angular.module('burybox')
