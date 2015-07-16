c328038c6493cd24e972697908adf61f6864548c
Kim Mattiko <amberlie@rocketmail.com>
2015-07-16 15:22:37 -0400

    attempting to save in firebase with no success yet

--- app/scripts/controllers/addphoto.js
+++ app/scripts/controllers/addphoto.js
@@ -3,19 +3,39 @@
   'use strict';
 
     angular.module('burybox')
-    .controller('PhotoController', function($scope) {
+    .controller('PhotoController',  PhotoController);
+
+    function PhotoController($scope, $firebaseArray, FIREBASE_URL, $firebaseObject, $stateParams) {
+      var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com/');
+      $scope.theContent = $firebaseArray(ref)
+
+      function($scope) {
       $scope.list = [];
-      $scope.someText = {
+      $scope.theContent = {
         testField: ''
       };
       // $scope.text = 'howdy';
       $scope.submit = function() {
-          $scope.list.push($scope.someText);
-          $scope.someText = { testField: ''};
+          $scope.list.push($scope.theContent);
+          $scope.theContent = { testField: ''};
         };
+      };
       });
 
-      
+
+    /*function($scope) {
+      $scope.list = [];
+      $scope.theContent = {
+        testField: ''
+      };
+      // $scope.text = 'howdy';
+      $scope.submit = function() {
+          $scope.list.push($scope.theContent);
+          $scope.theContent = { testField: ''};
+        };
+      });*/
+
+
       //on NG-CLick of submit button img needs to convert to
     //  console.log("test")
     //  var selectedFile = $('#input').get(0).file[0];
