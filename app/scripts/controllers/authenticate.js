3d3ae7c41617cea49c53e007dc57f4597e2f81f8
Kim Mattiko <amberlie@rocketmail.com>
2015-07-18 14:09:05 -0400

    trying to get email/pw sign in to work with help from @melrodbos

--- app/scripts/controllers/authenticate.js
+++ app/scripts/controllers/authenticate.js
@@ -2,7 +2,27 @@
 (function() {
   'use strict';
 
-  angular.module('burybox')
+  angular.module('burybox.services'['firebase'])
+
+  services.factory ('authServices', ['$state', '$timeout','$firebaseAuth'
+
+  function( $state, $timeout, $firebaseAuth)
+{
+  var authServices = {};
+  var auth = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+  authServices.authObj = $firebaseAuth(auth);
+  authServices.login = function (user, password) {
+    authServices.authObj.$authWithPassword({
+      email : user,
+      password : password
+    })
+    .then(function(response){
+      $state.go('addtobox.html')
+    })
+  }
+}
+  ])
+
 
   .controller('LoginController', function() {
     var firebase = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
@@ -17,3 +37,17 @@
     };
   });
 })();
+/*angular.module('burybox')
+
+.controller('LoginController', function() {
+  var firebase = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+
+  var self = this;
+
+  self.login = function() {
+//    firebase.authWithOAuthPopup('google', function(error, auth) {
+      //remember: 'sessionOnly';
+//    });
+
+//  };
+//}); */
