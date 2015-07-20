d962d59482587b3698090bf5e1d4197874bb8ce1
Kim Mattiko <amberlie@rocketmail.com>
2015-07-20 11:17:38 -0400

    naming controllers more clearly and updating index.html

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -52,3 +52,22 @@
   return authServices;
 }
   ]); */
+
+  /*(function() {
+    'use strict';
+
+    angular.module('burybox')
+
+    .controller('LoginController', function() {
+      var firebase = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+
+      var self = this;
+
+      self.login = function() {
+        firebase.authWithOAuthPopup('google', function() {
+          //remember: 'sessionOnly';
+        });
+
+      };
+    });
+  })();*/
