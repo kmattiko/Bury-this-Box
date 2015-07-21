e4d113225d8540d008e75fe349ab4fe52c7ab507
Kim Mattiko <amberlie@rocketmail.com>
2015-07-21 09:49:46 -0400

    got all of the errors fixed on page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,9 +7,7 @@
     .controller('LoginController', function() {
       var firebase = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
 
-      var self = this;
-
-      self.login = function() {
+      this.login = function() {
         firebase.authWithOAuthPopup('google', function() {
           //remember: 'sessionOnly';
         });
@@ -17,31 +15,3 @@
       };
     });
   })();
-  /*(function() {
-    'use strict';
-
-    angular.module('burybox')
-
-    .controller('LoginController', function($state, $timeout, $firebaseAuth, $location) {
-
-      var self = this;
-
-      self = {};
-
-      var auth = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-
-      self.authObj = $firebaseAuth(auth);
-      self.login = function() {
-        self.authObj.$authWithPassword({
-          email: self.user,
-          password: self.password
-        });
-        self.go = function() {
-          $location.path('/addtobox');
-        };
-
-      };
-
-
-        });
-  })(); /*
