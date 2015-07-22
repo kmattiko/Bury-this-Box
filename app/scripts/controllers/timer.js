e5b2aadb27516ff05f301a4566831dab2595dcaf
Kim Mattiko <amberlie@rocketmail.com>
2015-07-21 20:48:17 -0400

    got a start and stop button for timer

--- app/scripts/controllers/timer.js
+++ app/scripts/controllers/timer.js
@@ -6,6 +6,16 @@
 .controller('TimeController', function($scope){
 $scope.timerRunning = true;
 
+$scope.startTimer = function() {
+  $scope.$broadcast('timer-start');
+  $scope.timerRunning = true;
+};
+
+$scope.stopTimer = function() {
+  $scope.$broadcast('timer-stop');
+  $scope.timerRunning = false;
+};
+
 $scope.$on('timer-stopped', function() {
   alert("timer stopped")
 });
