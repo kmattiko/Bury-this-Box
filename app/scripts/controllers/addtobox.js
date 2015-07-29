b382f3e6ca01b02660aeeacae942c350e0b2f771
Kim Mattiko <amberlie@rocketmail.com>
2015-07-29 15:50:36 -0400

    getting the page to look better cloudinary option

--- app/scripts/controllers/addtobox.js
+++ app/scripts/controllers/addtobox.js
@@ -15,26 +15,7 @@
         return photo.url = result[0].url
       });
     }, false);
-      //var cloudinary = new Cloudinary('https://api.cloudinary.com/c1_1/kmattiko/image/upload');
-
-    /*  document.getElementById("upload_widget_opener").addEventListener("click", function() {
-      /*  cloudinary.openUploadWidget({ cloud_name: 'kmattiko', upload_preset: 'q5mpk6sc'}),
-        funcion(error, result) {
-          console.log(error, result)
-        };
-      }, false);*/
-      //  $http.post("https://api.cloudinary.com/c1_1/kmattiko/image/upload");
-
-       /*cloudinary.uploader.upload('upload_widget_opener') ({
-          cloud_name: 'kmattiko'
-          upload_preset: 'q5mpk6sc'
-        }),
-
-        /*function(error, results) {
-          console.log(result[0].url)
-          return photo.url = result[0].url
-        };*/
-    // }, false);
+  
 
       var firebase = new Firebase('https://sweltering-inferno-1762.firebaseio.com/');
 
