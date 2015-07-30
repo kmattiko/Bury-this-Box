8961c6ca419aaac86182a8104e006d57ef3b0cb9
Kim Mattiko <amberlie@rocketmail.com>
2015-07-30 15:55:21 -0400

    getting files organized better

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -2,36 +2,10 @@
 (function(){
 'use strict';
 
-var app = angular.module('burybox');
-
-app.controller('ShowHideController', function(){
+angular.module('burybox').controller('ShowHideController', function(){
 
   this.click = false;
+
 });
-
-/* TODO
-  When a timeframe is selected, enter button must be cliked
-  If/Else statement and Alert (Are you sure you want to hide this?)
-  On confirmation, NG Hide for images
-  also...NG Show for message "your images are hidden, come back later"
-  Timeframe starts counting
-  NG Hide/Show switches when timeframe ends
-*/
-/*var app = angular.module('burybox');
-app.controller('HideShowController', function(){
-  var hideshow = this;
-
-//_forEach src add ng-src? (how will img src have ng-src added to each?)
-
-
-  hideshow.submit(function(){
-    if( sumbit == true ) {
-      ng-hide (something) for (timeframe) && ng-show (something) for (timeframe)
-      .when (timeframe end) switch
-    }
-    else {
-      return false;
-    }
-  });*/
 
 })();
---
+++ app/scripts/controllers/logout.js
@@ -0,0 +1,8 @@
+/* global */
+(function(){
+'use strict';
+ angular.module('burybox').controller('LogoutController', function() {
+
+
+
+})();
