5c174208bed023b9913c3e08cd49dd092d3cd62b
Kim Mattiko <amberlie@rocketmail.com>
2015-07-30 11:01:02 -0400

    getting auth name to show and google info to firebase

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,7 +4,34 @@
 
     angular.module('burybox').controller('LoginController', function() {
 
-      var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+    var newUser = true;
+    var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+    var authData = ref.getAuth();
+    if (authData) {
+      console.log("you did it!", authData.uid);
+    }
+    this.login = function() {
+      ref.authWithOAuthPopup('google', function(error) {
+        if (error) {
+          console.log("failed", error);
+        } else {
+          console.log("done successfully", authData);
+        }
+      }, {
+        remember: 'sessionOnly'
+      });
+    };
+
+    ref.onAuth(function() {
+      if (authData && newUser) {
+        ref.child('user').child(authData.uid).set({
+          provider: authData.provider,
+          name: authData.google.displayName
+        });
+      }
+    });
+
+    /*  var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
       var authData = ref.getAuth();
       if(authData) {
         console.log("you did it!", authData.uid);
@@ -21,7 +48,7 @@
             remember: 'sessionOnly',
             scope: 'email'
           });
-      };
+      };*/
 
     });
 
