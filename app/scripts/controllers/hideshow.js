e646845a1b2686f1a44788105a4268503e74bdbc
Kim Mattiko <amberlie@rocketmail.com>
2015-07-31 18:48:26 -0400

    trying to tie the controller and the view

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -1,11 +1,17 @@
-/* global angular */
+/* global angular Firebase */
+
 (function(){
 'use strict';
-
-angular.module('burybox').controller('ShowHideController', function(){
-
-  this.click = false;
-
+console.log('hello to you too');
+angular.module('burybox').controller('HideShowController', function($firebase, $http){
+  console.log('test');
 });
+  console.log('hello there');
+  var hideshow = self;
+  var today = Date.now();
+  console.log(today);
+  var timestamp = $http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate');
+  console.log(timestamp);
+
 
 })();
