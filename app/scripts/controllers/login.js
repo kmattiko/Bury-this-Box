f786ebfedb362e04f9fad0da2ef47af2608167a6
Kim Mattiko <amberlie@rocketmail.com>
2015-07-31 15:01:08 -0400

    got my authentication working again, yay

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,34 +2,27 @@
   (function() {
     'use strict';
 
-    angular.module('burybox').controller('LoginController', function($location) {
+  angular.module('burybox').controller('LoginController', function($location, $firebase) {
 
-    var login = self
-    //var newUser = true;
-    var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-    login.auth = Auth.magicAuth;
-    login.authData = null;
-    login.addUser = function(){
-      login.auth.$onAuth(function(authData) {
-        loin.authData = authData;
-        if (authData) {
-          ref.child('users').child(authData.uid).update({
-            provider: authData.provider,
-            name: authData.google.displayName,
-            image: authData.google.profileImageURL
-          });
-        }
-      });
-    };
-    login.login = function() {
-      Auth.googleLogin();
-    };
-    login.checkAuth = function() {
-      console.log(Auth.authStatus());
-    };
-    login.logout = function() {
-      Auth.googleLogout();
-    };
+  var login = self;
+
+  var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
+  ref.authWithOAuthPopup('google', function(error, authData){
+    if (error) {
+      console.log('error');
+    } else {
+      console.log('authenticated', authData);
+    }
+  });
+    ref.onAuth(function(authData) {
+      if (authData) {
+        ref.child('user').child(authData.uid).update({
+          provider: authData.provider,
+          name: authData.google.displayName,
+          image: authData.google.profileImageURL
+        });
+      }
+    });
   });
 
   })();
