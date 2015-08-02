bf128faa36a1426236b9177fe605115ba98d9ac8
Kim Mattiko <amberlie@rocketmail.com>
2015-08-02 19:28:56 -0400

    trying to fix the header

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -2,16 +2,16 @@
 
 (function(){
 'use strict';
-console.log('hello to you too');
 angular.module('burybox').controller('HideShowController', function($firebase, $http){
-  console.log('test');
-});
-  console.log('hello there');
-  var hideshow = self;
-  var today = Date.now();
-  console.log(today);
-  var timestamp = $http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate');
+
+  var hideshow = this;
+
+  hideshow = ({
+  today: Date.now(),
+  timestamp: $http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate')
+  });
+
   console.log(timestamp);
-
-
+  console.log(today);
+});
 })();
