42c6af532af88cb7cc1975b85403fbf162cbd692
Kim Mattiko <amberlie@rocketmail.com>
2015-08-03 17:57:43 -0400

    trying to get the data to come back...still

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -3,18 +3,26 @@
 'use strict';
 angular.module('burybox').controller('HideShowController', function($firebase, $firebaseArray, $http){
 
-  var hideshow = this;
-  hideshow.opendate = [ ];
-  console.log(hideshow.opendate);
+//  var hideshow = this;
+//  hideshow.opendate = [ ];
+//  console.log(hideshow.opendate);
 
 //  var today = Date.now()
 
- //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google:112632700758187071933/opendate').then(function(response){
+ //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate').then(function(response){
    //return response;
  //});
  //hideshow.opendate = $firebaseArray(ref);
  console.log('hello');
 
+ var hideshow = this;
+ hideshow.opendate = [ ];
 
+ $http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate' + '.json')
+   .then(function(response) {
+     console.log(response);
+     hideshow.opendate = response.data;
+   });
+   console.log(hideshow.opendate)
 });
 })();
