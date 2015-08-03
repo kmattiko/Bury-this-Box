0d780bb5435460d02a9e49ca38e6446479c3ce41
Kim Mattiko <amberlie@rocketmail.com>
2015-08-03 15:25:45 -0400

    trying to get closer to having the opendate posting in console

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -1,14 +1,23 @@
 /* global angular Firebase */
 (function(){
 'use strict';
-angular.module('burybox').controller('HideShowController', function($firebase, $http){
+angular.module('burybox').controller('HideShowController', function($firebase, $firebaseArray, $http){
 
   var hideshow = this;
+  hideshow.opendate = [ ];
+  console.log(hideshow.opendate);
 
 //  var today = Date.now()
-  var timestamp = $http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate')
 
-  console.log(timestamp);
+ var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com/user');
+ hideshow.opendate = $firebaseArray(ref);
+ console.log(hideshow.opendate);
+//  console.log(ref);
+
+  //var authData = ref.getAuth();
+  //var timestamp = ref.data;
+
+  //console.log(timestamp);
 //  console.log(today);
 
 
