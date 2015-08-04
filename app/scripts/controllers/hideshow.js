ad5df02aa58ebee73ea7c98348fc65b13c807dc6
Kim Mattiko <amberlie@rocketmail.com>
2015-08-04 16:56:38 -0400

    trying to get the two pieces together

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -10,28 +10,15 @@
     ref.authWithOAuthPopup('google', function(error, authData){
       var userUid = authData.uid;
       console.log(userUid);
+      hideshow.opendate = [];
+      $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + userUid + '/opendate' + '.json');
+      //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json');
+      //  .then(function(response) {
+          console.log(response);
+          hideshow.opendate = response.data;
+          console.log(response.data);
+      //  });
     });
-  //  var auth = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-  //  var authData = auth.getAuth();
-  //  var authRef = auth.child(authData.uid);
-  //  console.log(authRef);
-  //  var authdData = googleUser;
-    //var googleUser = auth.child('user').child(auth.uid);
-    //var googleUser = auth.child('user').child(authUser.google.id);
-  //  console.log(authdData);
-    //('google', function(authData) {
-    //    console.log(authData);
-    //});
-    //  var hideshow = this;
-    //  hideshow.opendate = [ ];
-    //  console.log(hideshow.opendate);
-
-    //  var today = Date.now()
-
-    //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/opendate').then(function(response){
-    //return response;
-    //});
-    //hideshow.opendate = $firebaseArray(ref);
 
     hideshow.opendate = [];
     $http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json')
