14969a46e5d37cdfc8a5e936003d24e5f0f7a331
Kim Mattiko <amberlie@rocketmail.com>
2015-08-04 20:50:14 -0400

    got the timestamp to come back dynamically for each user

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -4,32 +4,18 @@
   angular.module('burybox').controller('HideShowController', function($firebase, $firebaseArray, $firebaseObject, $http) {
 
     var hideshow = this;
-
     var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
 
-    ref.authWithOAuthPopup('google', function(error, authData){
-      var userUid = authData.uid;
-      console.log(userUid);
-      hideshow.opendate = [];
-      $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + userUid + '/opendate' + '.json');
-      //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json');
-      //  .then(function(response) {
-          console.log(response);
-          hideshow.opendate = response.data;
-          console.log(response.data);
-      //  });
-    });
+    ref.authWithOAuthPopup('google', function(error, authData) {
 
-    hideshow.opendate = [];
-    $http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json')
+    $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/opendate' + '.json')
       //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json');
       .then(function(response) {
-        console.log(response);
         hideshow.opendate = response.data;
         console.log(response.data);
       });
 
-
+    });
 
   });
 })();
