7a0de74e4e9bd35fea1dda54bd4bde1a6f733576
Kim Mattiko <amberlie@rocketmail.com>
2015-08-05 14:15:16 -0400

    got the timestamp to come back correctly! woohoo

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -10,11 +10,12 @@
       //var userUid = authData.uid;
       //console.log(userUid);
 
-    $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/opendate' + '.json')
+    $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/timer' + '.json')
       //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json');
       .then(function(response) {
-        hideshow.opendate = response.data;
-        console.log(response.data);
+        hideshow.timer = response.data;
+        var timeToOpen = response.data.opendate;
+        console.log(timeToOpen);
       });
 
     });
