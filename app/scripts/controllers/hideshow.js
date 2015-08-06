cbbaba31f15a5e6b96cecddb9478d7e15482d394
Kim Mattiko <amberlie@rocketmail.com>
2015-08-06 08:32:42 -0400

    getting back to where it works locally at least

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -1,27 +1,27 @@
 /* global angular Firebase */
-(function(window) {
+(function() {
   'use strict';
   angular.module('burybox').controller('HideShowController', function($firebase, $http) {
 
     var hideshow = this;
-  //  hideshow.timer = { };
+    //  hideshow.timer = { };
     var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-    var timeToOpen;
     ref.authWithOAuthPopup('google', function(error, authData) {
       //var userUid = authData.uid;
       //console.log(userUid);
 
-    $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/timer' + '.json')
+      $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/timer' + '.json')
 
       .then(function setValue(response) {
         hideshow.timer = response.data;
-        timeToOpen = response.data.opendate;
+        var timeToOpen = response.data.opendate;
 
+        console.log(timeToOpen);
+        var today = Date.now();
+        console.log(today);
       });
     });
 
-      console.log(window.timeToOpen);
-      var today = Date.now();
-      console.log(today);
+
   });
-})(window);
+})();
