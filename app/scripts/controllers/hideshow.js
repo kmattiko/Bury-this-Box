ee1f97828903cc992cb7782902859f3e7e727937
Kim Mattiko <amberlie@rocketmail.com>
2015-08-06 16:09:08 -0400

    still trying to tie it somewhere visable

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -1,27 +1,25 @@
 /* global angular Firebase */
 (function() {
-  'use strict';
+
   angular.module('burybox').controller('HideShowController', function($firebase, $http) {
 
-    var hideshow = this;
-    //  hideshow.timer = { };
+    var self = this;
+
+    var endTime;
+
     var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-    ref.authWithOAuthPopup('google', function(error, authData) {
+    ref.authWithOAuthPopup('google', function (error, authData) {
       //var userUid = authData.uid;
       //console.log(userUid);
 
       $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/timer' + '.json')
-
-      .then(function setValue(response) {
-        hideshow.timer = response.data;
-        var timeToOpen = response.data.opendate;
-
-        console.log(timeToOpen);
-        var today = Date.now();
-        console.log(today);
+       .then(function(response) {
+        endTime = response.data.opendate;
+      //  console.log(endTime);
       });
     });
-
-
+    console.log(endTime);
+    var today = Date.now();
+    console.log(today);
   });
 })();
