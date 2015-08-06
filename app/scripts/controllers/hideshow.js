7e738eac5ac27e495b0a3cb23cc88dce8660e136
Kim Mattiko <amberlie@rocketmail.com>
2015-08-06 08:12:58 -0400

    tried to make several attempts at global variables

--- app/scripts/controllers/hideshow.js
+++ app/scripts/controllers/hideshow.js
@@ -1,28 +1,27 @@
 /* global angular Firebase */
-(function() {
+(function(window) {
   'use strict';
-  angular.module('burybox').controller('HideShowController', function($firebase, $firebaseArray, $firebaseObject, $http) {
+  angular.module('burybox').controller('HideShowController', function($firebase, $http) {
 
     var hideshow = this;
-    hideshow.timer = { };
+  //  hideshow.timer = { };
     var ref = new Firebase('https://sweltering-inferno-1762.firebaseio.com');
-
+    var timeToOpen;
     ref.authWithOAuthPopup('google', function(error, authData) {
       //var userUid = authData.uid;
       //console.log(userUid);
 
     $http.get('https://sweltering-inferno-1762.firebaseio.com/user/' + authData.uid + '/timer' + '.json')
-      //$http.get('https://sweltering-inferno-1762.firebaseio.com/user/google%3A112632700758187071933/opendate' + '.json');
 
-      .then(function(response) {
+      .then(function setValue(response) {
         hideshow.timer = response.data;
-        var timeToOpen = response.data.opendate;
-        console.log(timeToOpen);
-        var today = Date.now();
-        console.log(today);
-      });
+        timeToOpen = response.data.opendate;
 
+      });
     });
 
+      console.log(window.timeToOpen);
+      var today = Date.now();
+      console.log(today);
   });
-})();
+})(window);
